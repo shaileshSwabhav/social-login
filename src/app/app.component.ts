62662f5506dbee4007940bc913b550c2c5a0f9e8
shaileshSwabhav <shailesh@swabhavtechlabs.com>
2022-12-02 12:55:26 +0530

    new build in docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { SocialAuthService, FacebookLoginProvider } from '@abacritt/angularx-social-login';
+import { SocialAuthService, FacebookLoginProvider, SocialUser } from '@abacritt/angularx-social-login';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,17 +12,36 @@ export class AppComponent implements OnInit {
     private authService: SocialAuthService,
   ) { }
 
+  isLoggedIn: boolean = false
+
   ngOnInit(): void {
-    this.authService.authState.subscribe((user) => {
-      console.log(user);
-    }, (err: any) => {
-      console.error(err);
+    this.authService.authState.subscribe({
+      next: (user: SocialUser) => {
+        console.log("ngoninit user -> ", user);
+        this.isLoggedIn = true
+      },
+      error: (err: any) => {
+        console.error(err);
+        this.isLoggedIn = false
+      }
     })
   }
+
 
   signInWithFB(): void {
     console.log(FacebookLoginProvider.PROVIDER_ID);
 
-    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+    try {
+      const socialUser = this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+      console.log("after login -> ", socialUser);
+      this.isLoggedIn = true
+    } catch (error) {
+      console.error(error);
+      this.isLoggedIn = false
+    }
+  }
+
+  signout(): void {
+    this.authService.signOut();
   }
 }
